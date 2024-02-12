e3a309b5d2518e552ffa406be641eb6f29336b8a
Peal Hasan <95323270+hasanpeal@users.noreply.github.com>
2024-02-12 19:14:28 +0000

    test

--- src/hw1.c
+++ src/hw1.c
@@ -2,7 +2,69 @@
 
 void print_packet_sf(unsigned char packet[])
 {
-    (void)packet;
+    unsigned int src_addr = 0;
+    src_addr |= packet[0] << 20;
+    src_addr |= packet[1] << 12;
+    src_addr |= packet[2] << 4;
+    src_addr |= packet[3] >> 4;
+    printf("Source Address: %d\n", src_addr);
+    
+    unsigned int dest_addr = 0;
+    dest_addr |= (packet[3] << 4) << 24;
+    dest_addr |= packet[4] << 16;
+    dest_addr |= packet[5] << 8;
+    dest_addr |= packet[6];
+    printf("Destination Address: %d\n", dest_addr);
+    
+    unsigned int src_port = 0;
+    src_port |= packet[7] >> 4;
+    printf("Source Port: %d\n", src_port);
+    
+    unsigned int dest_port = 0;
+    dest_port |= (packet[7] & 0x0F);
+    printf("Destination Port: %d\n", dest_port);
+    
+    unsigned int fragment_offset = 0;
+    fragment_offset |= packet[8] << 6;
+    fragment_offset |= packet[9] >> 5;
+    printf("Fragment Offset: %d\n", fragment_offset);
+    
+    unsigned int packet_length = 0;
+    packet_length |= (packet[9] & 0x03) << 12;
+    packet_length |= packet[10] << 4;
+    packet_length |= (packet[11] & 0xF0) >> 4;
+    printf("Packet Length: %d\n", packet_length);
+    
+    unsigned int max_hop_count = 0;
+    max_hop_count |= (packet[11] & 0x0F) << 1;
+    max_hop_count |= (packet[12] & 0x80) >> 1;
+    max_hop_count = max_hop_count >> 2;
+    printf("Maximum Hop Count: %d\n", max_hop_count);
+    
+    unsigned int checksum = 0;
+    checksum |= (packet[12] & 0x7F) << 16;
+    checksum |= packet[13] << 8;
+    checksum |= packet[14];
+    printf("Checksum: %d\n", checksum);
+    
+    unsigned int compression_scheme = 0;
+    compression_scheme |= (packet[15] & 0xC0) >> 6;
+    printf("Compression Scheme: %d\n", compression_scheme);
+    
+    unsigned int traffic_class = 0;
+    traffic_class = packet[15] & 0x3F;
+    printf("Traffic Class: %d\n", traffic_class);
+    
+    printf("Payload: ");
+    for(int i = 16; i < packet_length; i += 4) {
+        int payload = (packet[i] << 24) | (packet[i + 1] << 16) | (packet[i + 2] << 8) | packet[i + 3];
+        if(i > 16) {
+            printf(" ");
+        }
+        printf("%d", payload);
+    }
+    printf("\n");
+    //(void)packet;
 }
 
 unsigned int compute_checksum_sf(unsigned char packet[])
@@ -38,3 +100,4 @@ unsigned int packetize_array_sf(int *array, unsigned int array_len, unsigned cha
     (void)traffic_class;
     return -1;
 }
+
