679b4baf10ae589ca970c8fa0e19be1e2bf4617b
Peal Hasan <95323270+hasanpeal@users.noreply.github.com>
2024-02-12 23:04:39 +0000

    Your commit message here

--- src/hw1.c
+++ src/hw1.c
@@ -68,8 +68,62 @@ void print_packet_sf(unsigned char packet[])
 
 unsigned int compute_checksum_sf(unsigned char packet[])
 {
-    (void)packet;
-    return -1;
+    unsigned int finalRes = 0;
+    unsigned long payloadSum = 0;
+    
+    unsigned int src_addr = 0;
+    src_addr |= packet[0] << 20;
+    src_addr |= packet[1] << 12;
+    src_addr |= packet[2] << 4;
+    src_addr |= packet[3] >> 4;
+    finalRes += src_addr;
+    
+    unsigned int dest_addr = 0;
+    dest_addr |= (packet[3] << 4) << 24;
+    dest_addr |= packet[4] << 16;
+    dest_addr |= packet[5] << 8;
+    dest_addr |= packet[6];
+    finalRes += dest_addr;
+    
+    unsigned int src_port = 0;
+    src_port |= packet[7] >> 4;
+    finalRes += src_port;
+    
+    unsigned int dest_port = 0;
+    dest_port |= (packet[7] & 0x0F);
+    finalRes += dest_port;
+    
+    unsigned int fragment_offset = 0;
+    fragment_offset |= (packet[8] & 0xFF) << 6;
+    fragment_offset |= (packet[9] & 0xFC) >> 2;
+    finalRes += fragment_offset;
+    
+    unsigned int packet_length = 0;
+    packet_length |= (packet[9] & 0x03) << 12;
+    packet_length |= packet[10] << 4;
+    packet_length |= (packet[11] & 0xF0) >> 4;
+    finalRes += packet_length;
+    
+    unsigned int max_hop_count = 0;
+    max_hop_count |= (packet[11] & 0x0F) << 1;
+    max_hop_count |= (packet[12] & 0x80) >> 7;
+    finalRes += max_hop_count;
+    
+    unsigned int compression_scheme = 0;
+    compression_scheme |= (packet[15] & 0xC0) >> 6;
+    finalRes += compression_scheme;
+    
+    unsigned int traffic_class = 0;
+    traffic_class = packet[15] & 0x3F;
+    finalRes += traffic_class;
+    
+    for(int i = 16; i < packet_length; i += 4) {
+        int payload = (packet[i] << 24) | (packet[i + 1] << 16) | (packet[i + 2] << 8) | packet[i + 3];
+        if(i > 16) payloadSum += abs(payload);
+    }
+    
+    unsigned int result = finalRes%8388607;
+    return result;
 }
 
 unsigned int reconstruct_array_sf(unsigned char *packets[], unsigned int packets_len, int *array, unsigned int array_len) {
