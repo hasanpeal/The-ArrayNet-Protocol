726e7c6d68be185cc7b2e779856cfbd52822b605
Peal Hasan <95323270+hasanpeal@users.noreply.github.com>
2024-02-13 15:57:24 +0000

    Your commit message here

--- src/hw1.c
+++ src/hw1.c
@@ -126,11 +126,40 @@ unsigned int compute_checksum_sf(unsigned char packet[])
 }
 
 unsigned int reconstruct_array_sf(unsigned char *packets[], unsigned int packets_len, int *array, unsigned int array_len) {
-    (void)packets;
-    (void)packets_len;
-    (void)array;
-    (void)array_len;
-    return -1;
+    unsigned int finalRes = 0;
+    for(int i = 0; i < packets_len; i++)
+    {
+        unsigned char *currPacket = packets[i];
+        unsigned int checksum1 = compute_checksum_sf(currPacket);
+        unsigned int checksum2 = 0;
+        checksum2 |= (currPacket[12] & 0x7F) << 16;
+        checksum2 |= currPacket[13] << 8;
+        checksum2 |= currPacket[14];
+        if(checksum1 != checksum2) continue;
+        unsigned int fragment_offset = 0;
+        fragment_offset |= (currPacket[8] & 0xFF) << 6;
+        fragment_offset |= (currPacket[9] & 0xFC) >> 2;
+        unsigned int targetIndex = fragment_offset/4;
+        if(targetIndex >= array_len) continue;
+        unsigned int packet_length = 0;
+        packet_length |= (currPacket[9] & 0x03) << 12;
+        packet_length |= currPacket[10] << 4;
+        packet_length |= (currPacket[11] & 0xF0) >> 4;
+        unsigned int totalPayload = (packet_length-16)/4;
+        for(int k = 0; (targetIndex + k) < array_len && k < totalPayload; k++)
+        {
+            int plIndex = (k*4) + 16;
+            int actualPl = (currPacket[plIndex] << 24) | (currPacket[plIndex+1] << 16) | (currPacket[plIndex+2] << 8) | (currPacket[plIndex+3]);
+            array[targetIndex + k] = actualPl;
+            finalRes++;
+        }
+    }
+    return finalRes;
+    // (void)packets;
+    // (void)packets_len;
+    // (void)array;
+    // (void)array_len;
+    // return -1;
 }
 
 unsigned int packetize_array_sf(int *array, unsigned int array_len, unsigned char *packets[], unsigned int packets_len,
